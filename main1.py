d701f582de6d9ba6d483600f2f23c3bb722a9a29
AA21Z <84468497+AA21Z@users.noreply.github.com>
2024-09-01 21:40:31 -0500

    Checkpoint of updates
    Minor updates to code logic.
    Added comments for readability and reusability of code.

--- main1.py
+++ main1.py
@@ -2,12 +2,35 @@ import os
 #from openai import OpenAI
 import whisper
 
+# Init full path instances TODO: AA to improve so only name of file is needed.
+# Get the current working directory
 working_directory = os.getcwd()
 
-model = whisper.load_model("base")
-result = model.transcribe(f"{working_directory}/Input Audio/AR_Sample.m4a")
+# Initialize the relative path to the sample audio file
+audio_loc = "/Input Audio/AR_Sample.m4a"
+full_audio_path = os.path.join(working_directory, audio_loc)
 
-print(result["text"])
+# Prompt the user to decide whether to run the sample file or their own file
+run_sample_input = input("Would you like to run the sample file? Yes or No: ").strip().lower()
+
+# Determine the file path based on user input
+if run_sample_input == "no":
+  full_audio_path = input("Please provide full audio path/location: ")
+else:
+  # if statement does not take into account any other inputs #TODO: AA Revist logic.
+  print("Running sample file now.")
+
+def transcribe_audio_whisper(source_file): 
+  model = whisper.load_model("base")
+  result = model.transcribe(source_file)
+  return result["text"]
+
+
+output_text = transcribe_audio_whisper(full_audio_path)
+
+print(output_text)
+
+# -- Older OpenAI code [Requires API key] -- 
 #client = OpenAI()
 
 #audio_file = open(f"{working_directory}/Input Audio/AR_Sample.m4a", "rb")

62e160d51b16dc81166e13896ed7be116ce998a9
AA21Z <84468497+AA21Z@users.noreply.github.com>
2025-06-17 22:25:32 -0500

    Create tkapp.py
    *Initializing tkinter GUI app to give user a GUI

---
+++ tkapp.py
@@ -0,0 +1,17 @@
+#From Geeks4geeks
+from tkinter import *
+
+
+# create root GUI window
+root = Tk()
+
+#Title and window dimension
+root.title("Meeting transcript App")
+# geometry (widthxheight)
+root.geometry('350x200')
+
+
+#### Widgets
+
+# Execute Tkinter
+root.mainloop()
